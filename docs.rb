9d0b95890bc0f65fba7d9b1fd618b24ab8e12c1f
Bruno Freitas <bmsatierf@gmail.com>
2014-09-14 10:22:28 -0300

    Move navigation and posxml routes to a new class

--- docs.rb
+++ docs.rb
@@ -1,3 +1,4 @@
+require_relative "routes"
 require "rubygems"
 require "sinatra"
 require "newrelic_rpm"
@@ -50,112 +51,26 @@ not_found do
   erb :not_found
 end
 
-# LOCALE
+# Locale
 post '/' do
   old_locale = params["url"].split("/")[1]
   redirect params["url"].sub(old_locale, params["locale"]), 301
 end
 
-# HOME
+# Landing page
 get "/" do redirect "/#{I18n.locale}/introduction" end
 
-@navigation = [
-  # OVERVIEW
-  { "url" => "introduction",                         "view_path" => "introduction/index"},
-  { "url" => "introduction/walk-framework",          "view_path" => "introduction/walk_framework"},
-  { "url" => "introduction/walk-manager",            "view_path" => "introduction/walk_manager"},
-  { "url" => "introduction/development-environment", "view_path" => "introduction/development_environment"},
-  { "url" => "introduction/posxml",                  "view_path" => "introduction/posxml"},
-  # WALK FRAMEWORK
-  { "url" => "walk-framework/verifone-evo",          "view_path" => "walk_framework/verifone_evo"},
-  { "url" => "walk-framework/verifone-evo-vx805",    "view_path" => "walk_framework/verifone_evo_vx805"},
-  { "url" => "walk-framework/verifone-verix",        "view_path" => "walk_framework/verifone_verix"},
-  { "url" => "walk-framework/ingenico-telium-1",     "view_path" => "walk_framework/ingenico_telium_1"},
-  { "url" => "walk-framework/ingenico-telium-2",     "view_path" => "walk_framework/ingenico_telium_2"},
-  { "url" => "walk-framework/first-launch",          "view_path" => "walk_framework/first-launch"},
-  { "url" => "walk-framework/configuration",         "view_path" => "walk_framework/configuration"},
-  # WALK MANAGER
-  { "url" => "walk-manager/apps",                    "view_path" => "walk_manager/apps"},
-  { "url" => "walk-manager/devices",                 "view_path" => "walk_manager/devices"},
-  { "url" => "walk-manager/groups",                  "view_path" => "walk_manager/groups"},
-  { "url" => "walk-manager/logical-numbers",         "view_path" => "walk_manager/logical_numbers"},
-  { "url" => "walk-manager/assets",                  "view_path" => "walk_manager/assets"},
-  { "url" => "walk-manager/transactions-monitor",    "view_path" => "walk_manager/transactions_monitor"},
-  # DEVELOPMENT ENVIRONMENT
-  { "url" => "development-environment/overview",     "view_path" => "development_environment/overview"},
-  { "url" => "development-environment/emulator",     "view_path" => "development_environment/emulator"},
-  # POSXML
-  { "url" => "posxml/structure",                     "view_path" => "posxml/structure"},
-  { "url" => "posxml/memory-and-variables",          "view_path" => "posxml/memory_and_variables"},
-  { "url" => "posxml/file-system",                   "view_path" => "posxml/file_system"},
-  { "url" => "posxml/configuration",                 "view_path" => "posxml/configuration"},
-  { "url" => "posxml/commands",                      "view_path" => "posxml/commands"},
-  # DEVELOPER API
-  { "url" => "api/overview",                         "view_path" => "api/overview"},
-  { "url" => "api/v1/apps",                          "view_path" => "api/v1/apps"},
-  { "url" => "api/v1/devices",                       "view_path" => "api/v1/devices"},
-  { "url" => "api/v1/logical-numbers",               "view_path" => "api/v1/logical_numbers"},
-  { "url" => "api/v1/groups",                        "view_path" => "api/v1/groups"},
-  { "url" => "api/v1/parameters",                    "view_path" => "api/v1/parameters"},
-  { "url" => "api/v1/assets",                        "view_path" => "api/v1/assets"},
-  { "url" => "api/v1/users",                         "view_path" => "api/v1/users"},
-  { "url" => "api/v1/logs",                          "view_path" => "api/v1/logs"},
-  # INTEGRATION
-  { "url" => "integration/architecture",             "view_path" => "integration/architecture"},
-  { "url" => "integration/tcp",                      "view_path" => "integration/tcp"},
-  { "url" => "integration/http",                     "view_path" => "integration/http"},
-  { "url" => "integration/advanced-http",            "view_path" => "integration/advanced_http"}
-]
-
-@navigation.each do |item|
-  get "/#{item["url"]}" do
-    redirect "/#{I18n.locale}/#{item["url"]}", 301
-  end
+# Navigation pages
+Routes.navigation.each do |item|
+  get "/#{item["url"]}" do redirect "/#{I18n.locale}/#{item["url"]}", 301 end
   get "/:locale/#{item["url"]}" do |locale|
     @params = request.env['rack.request.query_hash']
     erb "#{item["view_path"]}".to_sym
   end
 end
 
-# COMMANDS DESCRIPTIONS
-@commands = [
-  # flow
-  "if", "else", "while", "break", "function", "callfunction", "execute", "exit",
-  # readcard
-  "getcardvariable", "system.readcard", "system.inputtransaction",
-  # ui
-  "menu", "menuwithheader", "displaybitmap", "display", "cleandisplay", "system.gettouchscreen",
-  # print
-  "print", "printbig", "printbitmap", "printbarcode", "checkpaperout", "paperfeed",
-  # input
-  "inputfloat", "inputformat", "inputinteger", "inputoption", "inputmoney",
-  # crypto
-  "crypto.crc", "crypto.encryptdecrypt", "crypto.lrc", "crypto.xor",
-  # file
-  "filesystem.listfiles", "filesystem.renamefile", "filesystem.filesize", "filesystem.space", "file.open", "file.read", "file.write", "file.close", "readfile", "readfilebyindex", "editfile", "deletefile", "downloadfile",
-  # iso
-  "iso8583.initfieldtable", "iso8583.initmessage", "iso8583.putfield", "iso8583.endmessage", "iso8583.transactmessage", "iso8583.analyzemessage", "iso8583.getfield",
-  # serialport
-  "openserialport", "writeserialport", "readserialport", "closeserialport",
-  # datetime
-  "getdatetime", "time.calculate", "adjustdatetime",
-  # conectivity
-  "predial", "preconnect", "shutdownmodem", "network.checkgprssignal", "network.hostdisconnect", "network.ping", "network.start", "network.send", "network.receive",
-  # pinpad
-  "pinpad.open", "pinpad.loadipek", "pinpad.getkey", "pinpad.getpindukpt", "pinpad.display", "pinpad.close",
-  # emv
-  "emv.open", "emv.loadtables", "emv.cleanstructures", "emv.adddata", "emv.getinfo", "emv.inittransaction", "emv.processtransaction", "emv.finishtransaction", "emv.removecard", "emv.settimeout", "system.readcard", "system.inputtransaction",
-  # variables
-  "integervariable", "stringvariable", "integerconvert", "convert.toint", "inttostring", "stringtoint", "integeroperator", "string.tohex", "string.fromhex",
-  # string
-  "string.charat", "string.elementat", "string.elements", "string.find", "string.getvaluebykey", "string.trim", "string.insertat", "string.length", "string.pad", "string.removeat", "string.replace", "string.replaceat", "string.substring", "substring", "joinstring", "input.getvalue",
-  # smartcard
-  "smartcard.insertedcard", "smartcard.closereader", "smartcard.startreader", "smartcard.transmitapdu",
-  # utils
-  "mathematicaloperation", "system.beep", "system.backlight", "system.checkbattery", "system.info", "system.qrcode", "system.restart", "unzipfile", "waitkey", "waitkeytimeout", "readkey", "wait"
-]
-
-@commands.each do |command|
+# POSXML commands
+Routes.commands.each do |command|
   get "/posxml/commands/#{command}" do redirect "/#{I18n.locale}/posxml/commands/#{command}", 301 end
   get "/:locale/posxml/commands/#{command}" do |locale|
     erb "posxml/commands/#{command}".to_sym
