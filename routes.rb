81148d5b23bf0c4588aaca53d5e6f708363c6f55
Daniel Rodríguez <sadasant@gmail.com>
2015-02-02 15:26:34 -0500

    first page of the CLI docs after the intro: installing the CLI

--- routes.rb
+++ routes.rb
@@ -54,6 +54,7 @@ class Routes
       { "url" => "guides/transactions-patterns/patterns-chart",          "view_path" => "guides/transactions_patterns/tutorial_3_patterns_chart"},
       { "url" => "guides/transactions-patterns/notifications-settings",  "view_path" => "guides/transactions_patterns/tutorial_4_notifications_settings"},
       { "url" => "guides/cli",                                           "view_path" => "guides/cli/intro"},
+      { "url" => "guides/cli/set-up",                                    "view_path" => "guides/cli/tutorial_1_set_up"},
     ]
   end
 
