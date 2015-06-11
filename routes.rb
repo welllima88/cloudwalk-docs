5677f66ea12af5583d3aa282666160026a08bf06
Bruno Freitas <bmsatierf@gmail.com>
2015-06-11 10:07:11 -0300

    Add basic structure

--- routes.rb
+++ routes.rb
@@ -54,6 +54,7 @@ class Routes
       { "url" => "guides/transactions-patterns/anomalies-report",        "view_path" => "guides/transactions_patterns/tutorial_2_anomalies_report"},
       { "url" => "guides/transactions-patterns/patterns-chart",          "view_path" => "guides/transactions_patterns/tutorial_3_patterns_chart"},
       { "url" => "guides/transactions-patterns/notifications-settings",  "view_path" => "guides/transactions_patterns/tutorial_4_notifications_settings"},
+      { "url" => "guides/emulator-card-swipe",                           "view_path" => "guides/emulator_card_swipe"},
       #HELP
       { "url" => "help/faq",                                             "view_path" => "help/faq"},
     ]
